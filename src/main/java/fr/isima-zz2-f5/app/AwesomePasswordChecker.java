1e665c2d1231acedeb70ba0532e934f9da9821e5
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-11-06 14:21:27 +0100

    Added missing java dependancies

--- src/main/java/fr/isima-zz2-f5/app/AwesomePasswordChecker.java
+++ src/main/java/fr/isima-zz2-f5/app/AwesomePasswordChecker.java
@@ -6,6 +6,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import java.util.ArrayList;
 import java.util.List;
